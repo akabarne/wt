683351a59821c80ffa0ac00d0b486b9755ea7f6e
Werner Bock <wernerbock@Air-von-Werner.lan>
2022-12-02 20:52:53 +0100

    init co

---
+++ _attachments/js/index.js
@@ -0,0 +1,40 @@
+console.log("gehts?")
+console.log($("#weight").val())
+console.log($("#weight"))
+
+
+const srv = "http://5.45.102.31:5984/wt"
+//http://5.45.102.31:5984/wt/_design/wt/_view/weightdate
+const date_weight_path = "/_design/wt/_view/weightdate"
+console.log ($.get(srv + date_weight_path))
+
+getDate = function(){
+    const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+return `${day}-${month}-${year}`
+
+};
+
+
+$("#addWeight").click(function(){
+    $.ajax({
+        url: srv,
+        type: 'post',
+        data: JSON.stringify({
+            weight: $("#weight").val(), 
+            date: getDate()
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.info(data);
+        }
+    })
+    
+})
+
